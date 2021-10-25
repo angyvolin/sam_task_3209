0c5958dc28ebc34f41d584ad4a561ec62042f9b4
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2021-10-26 00:37:05 +0300

    Added a log entry to the file file.

--- src/main/java/sam/com/Main.java
+++ src/main/java/sam/com/Main.java
@@ -1,16 +1,25 @@
 package src.main.java.sam.com;
 
 import src.main.java.sam.com.controller.Controller;
-
+import src.main.java.sam.com.model.WriteHelper;
 
 
 public class Main {
+    private static final String LOG_TXT = "log.txt";
 
-  public static void main(String[] args) {
-          View view = View.builder().build();
-          Controller controller = Controller.builder().view(view).build();
-          view.setController(controller);
-          view.init();
-          controller.init();
-      }
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            WriteHelper.setPath(args[0]);
+        } else {
+            String path = System.getProperty("user.dir");
+            WriteHelper.setPath(System.getProperty("user.dir") + LOG_TXT);
+        }
+
+        View view = View.builder().build();
+        Controller controller = Controller.builder().view(view).build();
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
 }
---
+++ src/main/java/sam/com/model/WriteHelper.java
@@ -0,0 +1,41 @@
+package src.main.java.sam.com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WriteHelper {
+    private static boolean flagStart = true;
+    private static String path = "";
+
+    public static void setPath(String path) {
+        WriteHelper.path = path;
+    }
+
+
+    public static void writeLog(String in) {
+        if (flagStart && new File(path).isFile()) {
+            new File(path);
+            flagStart = false;
+        }
+        write(in);
+    }
+
+    private static void write(String in) {
+        try (FileWriter writer = new FileWriter(path, false)) {
+            writer.write(in);
+            writer.append('\n');
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
