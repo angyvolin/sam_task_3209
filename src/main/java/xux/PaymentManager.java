07a44ff94c638c476c6aecfae1d21cf6ddbfae87
Samuil Olegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2022-10-22 13:03:24 +0300

    Add files via upload

---
+++ src/main/java/xux/PaymentManager.java
@@ -0,0 +1,24 @@
+package com.samuilolegovich.model.PaymentManager.interfaces;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface PaymentManager {
+    void sendPayment(String address, Integer tag, BigDecimal numberOfXRP, boolean isReal);
+    void setterWallet(boolean isReal);
+    void updateWallet(boolean isReal);
+
+    Map<String, String> connectAnExistingWallet(String seed, boolean isReal);
+    Map<String, String> createNewWallet(boolean isReal);
+
+    String getClassicAddress(boolean isReal);
+    String getPrivateKey(boolean isReal);
+    String getPublicKey(boolean isReal);
+    String getXAddress(boolean isReal);
+    String getSeed(boolean isReal);
+
+    BigDecimal getAllBalance(boolean isReal);
+    BigDecimal getBalance(boolean isReal);
+
+    boolean isTest(boolean isReal);
+}
